16c26a067d0f4d12178bbc564f82b63ba6b87f62
CircumFlexing Coon <baumgaertner997@gmx.de>
2022-11-26 21:20:30 +0100

    implement MediaPlayer along with internal method for handling the case of one and/or two parameters

--- src/main/java/AudioPlayer.java
+++ src/main/java/AudioPlayer.java
@@ -1,10 +1,27 @@
-public class AudioPlayer implements MusicPlayer {
+public class AudioPlayer implements MediaPlayer {
+
+    public void play(String fileName) {
+        if(fileName.contains("mp3")) {
+            System.out.println("Playing mp3 file: " + fileName);
+        } else if (fileName.contains("vlc")) {
+            System.out.println("Playing Vlc file: " + fileName);
+        } else if (fileName.contains("mp4")) {
+            System.out.println("Playing mp4 file: " + fileName);
+        } else {
+            System.out.println("Type of audio not supported, filename: " + fileName);
+        }
+    }
+
     @Override
-    public void playMusic(String audioType, String fileName) {
+    public void play(String audioType, String fileName) {
         if(audioType.equalsIgnoreCase("mp3")) {
             System.out.println("Playing mp3 file: " + fileName);
+        } else if (audioType.equalsIgnoreCase("vlc")) {
+            System.out.println("Playing Vlc file: " + fileName);
+        } else if (audioType.equalsIgnoreCase("mp4")) {
+
         } else {
-            System.out.println("Audio type not supported for the file: " + fileName);
+            System.out.println("Type of audio not supported: " + audioType);
         }
     }
 }
