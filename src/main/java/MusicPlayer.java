115224714910020c6585540c8c33088adfb906f4
santhoshvanaparthi <116310564+santhoshvanaparthi@users.noreply.github.com>
2022-11-25 20:58:48 -0500

    updated mediaplayer by adding adapter pattern

--- src/main/java/MusicPlayer.java
+++ src/main/java/MusicPlayer.java
@@ -1,3 +1,28 @@
 public interface MusicPlayer {
     void playMusic(String audioType, String fileName);
 }
+
+public class  MediaPlayer {
+
+
+   public mediaplayer(String audioType){
+   
+      if(audioType.equalsIgnoreCase("vlc") ){
+         advancedMusicPlayer = new VlcPlayer();			
+         
+      }else if (audioType.equalsIgnoreCase("mp4")){
+         advancedMusicPlayer = new Mp4Player();
+      }	
+   }
+
+   @Override
+   public void play(String audioType, String fileName) {
+   
+      if(audioType.equalsIgnoreCase("vlc")){
+         advancedMusicPlayer.playVlc(fileName);
+      }
+      else if(audioType.equalsIgnoreCase("mp4")){
+         advancedMusicPlayer.playMp4(fileName);
+      }
+   }
+}
