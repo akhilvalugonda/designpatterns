f25a9d327ede60c61cdcddf3f02aebc9dc8732ba
dobbalaash <91915625+dobbalaash@users.noreply.github.com>
2022-11-28 12:37:06 -0500

    Update Driver.java

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -1,14 +1,10 @@
 public class Driver {
     public static void main(String[] args) {
-        AudioPlayer audioPlayer = new AudioPlayer();
-        audioPlayer.playMusic("mp3", "song1.mp3");
-
-        Mp4Player mp4MusicPlayer = new Mp4Player();
-        mp4MusicPlayer.playMp4Player("song2.mp4");
-
-        VlcPlayer vlcMusicPlayer = new VlcPlayer();
-        vlcMusicPlayer.playVlcPlayer("song3.vlc");
-
-        
+        AdapterMedia ad = new AdapterMedia();
+        ad.playMusic("mp4", "song2.mp4");
+        ad.playMusic("mp3", "song1.mp3");
+        ad.playMusic("wma", "song4.wma");
+        ad.playMusic("vlc", "song3.vlc");
+       
     }
 }
