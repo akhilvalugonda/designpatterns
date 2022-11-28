975a6ce2d443a768686a5d62b843828f5b8de8d4
santhoshvanaparthi <116310564+santhoshvanaparthi@users.noreply.github.com>
2022-11-27 22:43:35 -0500

    updated Audioplayer.java
    Here , created an concrete class for the implementation of this interface in Mediaplayer.

--- src/main/java/AudioPlayer.java
+++ src/main/java/AudioPlayer.java
@@ -3,8 +3,27 @@ public class AudioPlayer implements MusicPlayer {
     public void playMusic(String audioType, String fileName) {
         if(audioType.equalsIgnoreCase("mp3")) {
             System.out.println("Playing mp3 file: " + fileName);
-        } else {
+        } 
+        
+        else if {audioType.equalsIgnorecase("Vlc") audioType.isequalstothecase("mp4")){
+            Mediaadapter = new media(audiotype);
+            Mediaadapter.play(audioType,fileName);
+        
+        
+        }
+        
+        
+        else {
+           
             System.out.println("Audio type not supported for the file: " + fileName);
         }
+        
+        
+        
+        
+        
     }
+    
+    
+    
 }
